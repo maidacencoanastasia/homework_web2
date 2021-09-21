72c2f07a170f8228baf88e5c4f58898a58079788
maidacencoanastasia <maidacenco100@gmail.com>
2021-09-21 19:41:18 +0300

    ex3 c done

--- src/main/java/com/example/demo/controller/Ex3.java
+++ src/main/java/com/example/demo/controller/Ex3.java
@@ -2,17 +2,16 @@ package com.example.demo.controller;
 
 import com.example.demo.mathOperators.Add;
 import com.example.demo.mathOperators.Concat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/ex3")
 public class Ex3 {
-    @GetMapping("/ex3-add")
+    @PostMapping("/add")
     public Integer add(@RequestBody Add numbers) {
         return numbers.a + numbers.b;
     }
-    @GetMapping("/ex3-concat")
+    @PostMapping("/concat")
     public String concat(@RequestBody Concat concat ) {
         return concat.a + concat.b;
     }
