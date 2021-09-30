edf7b6b624f9067b6998308f343f0db023d5f960
maidacencoanastasia <maidacenco100@gmail.com>
2021-09-30 22:35:06 +0300

    add mustache (i chose book, because i want to implement an online library)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.example.demo;
-
+import com.samskivert.mustache.Mustache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mustache.MustacheEnvironmentCollector;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,6 +12,20 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("funcicleaza");
+	}
+	@Bean
+	public Mustache.Compiler mustacheCompiler(
+			Mustache.TemplateLoader templateLoader,
+			Environment environment) {
+
+		MustacheEnvironmentCollector collector
+				= new MustacheEnvironmentCollector();
+		collector.setEnvironment(environment);
+
+		return Mustache.compiler()
+				.defaultValue("Some Default Value")
+				.withLoader(templateLoader)
+				.withCollector(collector);
 	}
 
 }
